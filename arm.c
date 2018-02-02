63da20f2679c9977024872328ee048282aeb2acb
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-02-02 16:41:47 +0530

    arm.c
    arm

---
+++ arm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int digit,a,b,i,sum,num;
+printf("Enter the two num");
+scanf("%d%d",&a,&b);
+for(i=a;i<=b;i++)
+{
+	sum=0;
+	num=i;
+while(num!=0)
+{
+digit=num%10;
+sum=sum+digit*digit*digit;
+num=num/10;
+}
+if(sum==i)
+{
+printf("the num is%d",i);
+}
+}
+return 0;
+}
